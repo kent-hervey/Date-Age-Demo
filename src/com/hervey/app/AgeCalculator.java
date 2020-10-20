1488a83133cff2d38708532fbf4486e9c65d0d0d
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-10-20 13:27:49 -0500

    Added javadoc

--- src/com/hervey/app/AgeCalculator.java
+++ src/com/hervey/app/AgeCalculator.java
@@ -4,35 +4,33 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.time.Period;
 
+/**
+ * Calculates age given LocalDate birthdate and LocalDate now
+ * @author Kent
+ *
+ */
 public class AgeCalculator {
 
 	LocalDate birthDate = LocalDate.of(1930, Month.JANUARY, 1);
 	LocalDate today = LocalDate.now();
-	
-	
+
 	public static int calculateAge() {
-		LocalDate sampleBirthDate =LocalDate.of(1925, Month.NOVEMBER, 26);
+		LocalDate sampleBirthDate = LocalDate.of(1925, Month.NOVEMBER, 26);
 		LocalDate currentDate = LocalDate.now();
-		
+
 		if ((sampleBirthDate != null) && (currentDate != null)) {
 			return Period.between(sampleBirthDate, currentDate).getYears();
-		} 
-		else {
+		} else {
 			return 0;
 		}
 	}
-	
-	
 
 	public static int calculateAge(LocalDate birthDate, LocalDate currentDate) {
 		if ((birthDate != null) && (currentDate != null)) {
 			return Period.between(birthDate, currentDate).getYears();
-		} 
-		else {
+		} else {
 			return 0;
 		}
 	}
-	
-	
 
 }
