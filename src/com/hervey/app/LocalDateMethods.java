339c621bff027783804c766653c0c2ab46db1173
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-10-20 13:38:45 -0500

    added additional methods

--- src/com/hervey/app/LocalDateMethods.java
+++ src/com/hervey/app/LocalDateMethods.java
@@ -21,8 +21,12 @@ public class LocalDateMethods {
 		LocalDate sampleDate = LocalDate.of(1925, Month.NOVEMBER, 26);
 		System.out.println("Standard output of Sample Date is:  " + sampleDate); 
 		
-		System.out.println(sampleDate.lengthOfYear());
 
+		System.out.println("Month of this sampleDate is:  " + sampleDate.getMonth());
+		System.out.println("Year of this sampleDate is:  " + sampleDate.getYear());
+		System.out.println("Day number in this year is " + sampleDate.getDayOfYear());
+		System.out.println("Length of this year in days is:  " + sampleDate.lengthOfYear());	
+		System.out.println("year consumed to this sampleDate is:  " + ((double)  sampleDate.getDayOfYear()/sampleDate.lengthOfYear()));
 
 
 		
