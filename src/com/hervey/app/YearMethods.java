88beee96e162e163d5726b4f6dee0a8774ccf58e
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-10-20 13:38:25 -0500

    Added enclosing method

--- src/com/hervey/app/YearMethods.java
+++ src/com/hervey/app/YearMethods.java
@@ -4,11 +4,14 @@ import java.time.Year;
 
 public class YearMethods {
 	
+	public static void demoYear() {
+	
 	boolean isLeap = Year.isLeap(1961);
 	System.out.println("is isleap a leap year?  " + isLeap);
 	
 	System.out.println("instance of year:  " + Year.of(1960));
 	
 	String stringOfYear = Year.of(1960).toString();
+	}
 
 }
